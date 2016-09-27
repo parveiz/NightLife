0bf73362908a6f700da51c7c708a701f5a20bdcc
Parvez Khan <parveizkhan@gmail.com>
2016-09-27 16:04:16 +0300

    propertymodel

--- controller/homeController.php
+++
@@ -1,3 +0,0 @@
-<?php
-require "model/connect.php";
-
--- controller/propertyController.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-include_once ("../model/PropertyControllerClass.php");
-
-print_r($_POST);
-$p = new PropertyControllerClass();
-$p->addProperty();
